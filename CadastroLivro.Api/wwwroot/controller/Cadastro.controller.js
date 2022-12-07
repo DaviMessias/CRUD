8d9ebf4ac15ae94be0f6dbcec024628569fa53f0
DaviMessias <davibastos451@gmail.com>
2022-12-07 13:54:48 -0300

    concluido etapas basicas

--- CadastroLivro.Api/wwwroot/controller/Cadastro.controller.js
+++ CadastroLivro.Api/wwwroot/controller/Cadastro.controller.js
@@ -9,14 +9,22 @@
     
     return Controller.extend("sap.ui.demo.walkthrough.controller.Cadastro", {
         onInit: function () {
-
 			var rotaDaView = sap.ui.core.UIComponent.getRouterFor(this);
 			rotaDaView.attachRoutePatternMatched(this._aoCoincidirObjeto, this);
+
+			var oView = this.getView(),
+			oMM = Core.getMessageManager();
+
+			oMM.registerObject(oView.byId("input-nome"), true);
+			oMM.registerObject(oView.byId("input-autor"), true);
+			oMM.registerObject(oView.byId("input-editora"), true);
 		},
 
 		_aoCoincidirObjeto : function (oEvent) {
-
-			if (oEvent.getParameter("name") == "cadastroDeLivros") {
+			if (oEvent.getParameter("name") == "editar") {
+				var salvarId = window.decodeURIComponent(oEvent.getParameter("arguments").id);
+				this.exibirLivro(salvarId);
+			} else {
 				let modeloLivro = new JSONModel({
 					nome: "",
 					autor: "",
@@ -24,29 +32,64 @@
 					data: "",
 				});
 				this.getView().setModel(modeloLivro, "Livro");
-
-			} else {
-				var salvarId = window.decodeURIComponent(oEvent.getParameter("arguments").id);
-				this.exibirLivro(salvarId);
+				 
+				this.byId("DP6").setMinDate(new Date(1800, 0, 1));
+			 	this.byId("DP6").setMaxDate(new Date);
+			 	this.byId("DP6").setDateValue(new Date);
 			}
 		},
 
-		exibirLivro : function (idLivro){
+		validarInput: function (oInput) {
+			var valorStatus = "None";
+			var booleanValidacao = false;
+			var oBinding = oInput.getBinding("value");
 
-			this.buscarLivro(idLivro)
-				.then(livroDoBanco => {
-				let livro = new JSONModel(livroDoBanco)
-				this.getView().setModel(livro, "Livro")
-			})
+			try {
+				oBinding.getType().validateValue(oInput.getValue());
+			} catch (oException) {
+				valorStatus = "Error";
+				booleanValidacao = true;
+			}
+
+			oInput.setValueState(valorStatus);
+
+			return booleanValidacao;
 		},
 
-		buscarLivro: async function(idLivro){
-			
-			return await fetch(`https://localhost:7187/api/livro/${idLivro}`)
-			.then(res => res.json())
+		aoSalvar: function () {
+			// coleta os controles de entrada
+			var oView = this.getView(),
+				aInputs = [
+				oView.byId("input-nome"),
+				oView.byId("input-autor"),
+				oView.byId("input-editora"),
+				
+			],
+				booleanValidacao = false;
+
+			// verifica se as entradas não estão vazias.
+			// A validação não ocorre durante a vinculação de dados, pois ela é acionada apenas por ações do usuário.
+
+			aInputs.forEach(function (oInput) {
+				booleanValidacao = this.validarInput(oInput) || booleanValidacao;
+			}, this);
+
+			if (!booleanValidacao) {
+
+				if(!oView.byId("DP6").isValidValue()){
+					MessageBox.alert("Data Inválida");
+				 }
+				 
+			} else {
+				MessageBox.alert("Ocorreu um erro de validação. Complete todos os campos primeiro.");
+			}
 		},
 		
 		aoPressionarSalvar: async function () {
+
+			this.aoSalvar()
+			this.validarInput();
+			
 			let livro = this.getView().getModel("Livro").getData();
 			if(!livro.id){
 				this.criarLivro();
@@ -54,15 +97,11 @@
 				this.editarLivro();
 			}
 			var oRouter = this.getOwnerComponent().getRouter();
-
 			oRouter.navTo("lista");
-
-
 		},
 
 		criarLivro : async function (){
 			const idCampoData = "walkthrough---cadastroDeLivros--DP6";
-
 			let livro = this.getView().getModel("Livro").getData();
 			livro.data = new Date(this.byId(idCampoData)
 				.getValue())
@@ -81,7 +120,6 @@
 		},
 
 		editarLivro : async function() {
-
 			let livroASerEditado = this.getView().getModel("Livro").getData();
 			let idLivroParaEdicao = livroASerEditado.id;
 
@@ -97,68 +135,38 @@
 
 			MessageToast.show("Usuário editado")
 		},
-			
-		dataMaxMin : function(){
-			
-			// this.byId("DP6").setMinDate(new Date(1800, 0, 1));
-			// this.byId("DP6").setMaxDate(new Date);
-			// this.byId("DP6").setDateValue(new Date);
+
+		exibirLivro : function (idLivro){
+			this.buscarLivro(idLivro)
+				.then(livroDoBanco => {
+				let livro = new JSONModel(livroDoBanco)
+				this.getView().setModel(livro, "Livro")
+			})
+		},
+
+		buscarLivro: async function(idLivro){
+			return await fetch(`https://localhost:7187/api/livro/${idLivro}`)
+			.then(res => res.json())
 		},
 	
 		aoClicarEmBotaoVoltar: function(){
 				var oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("lista");
+			},
+		
+		validar : function(){
+			let livro = this.getView().getModel("Livro").getData();
+
+			if (livro.nome == '' ||livro.autor== ''  ||livro.editora == '' || livro.data == '' ) {
+					MessageBox.alert("Ocorreu um erro de validação. Complete todos os campos primeiro.");
+					livro.focus();
 			}
+		},
+
+
 	});
 });
 
 
 
 
-
-// validarInput: function (oInput) {
-// 	var sValueState = "None";
-// 	var bErroNaValidacao = false;
-// 	var oBinding = oInput.getBinding("value");
-
-// 	try {
-// 		oBinding.getType().validateValue(oInput.getValue());
-// 	} catch (oException) {
-// 		sValueState = "Error";
-// 		bErroNaValidacao = true;
-// 	}
-
-// 	oInput.setValueState(sValueState);
-
-// 	return bErroNaValidacao;
-// },
-
-
-
-// aoMudarDeCampo: function(oEvent) {
-// 	var oInput = oEvent.getSource();
-// 	this.validarInput(oInput);
-// },
-
-
-// coleta controles de entrada
-// var objetoView = this.getView(),
-// aInputs = [
-// objetoView.byId("input-id"),
-// objetoView.byId("input-titulo"),
-// objetoView.byId("input-autor"),
-// objetoView.byId("input-editora")
-// ],
-// bErroNaValidacao = false;
-
-// // Verifique se as entradas não estão vazias.
-// // A validação não ocorre durante a vinculação de dados, pois ela é acionada apenas por ações do usuário.
-// aInputs.forEach(function (oInput) {
-// bErroNaValidacao = this.validarInput(oInput) || bErroNaValidacao;
-// }, this);
-
-// if (!bErroNaValidacao) {
-// MessageToast.show("Livro adicionado com sucesso");
-// } else {
-// MessageBox.alert("Ocorreu um erro de validação. Complete todos os campos primeiro.");
-// }
